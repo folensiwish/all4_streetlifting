82b97391cc5803168b5b46140ad0e9ca1ec75934
folensiwish <fer.albertoandres@gmail.com>
2026-03-25 14:08:14 -0300

    Configuracion del aplicativo: 1.integrar postgresql 2.crear las app del proyecto 3.estructurar el proyecto con buenas practicas, creando carpeta apps para contener los aplicativos, templates en raiz de apps conteniendo las carpetas de sus applicaciones asi con js y static, manteniendo un init y base.html en la raiz de apps. 4. Modificar archivos settings para dirigir django a los directorios nuevos creados, ademas de separar las INSTALLED_APPS en varias listas -> apps_creadas, apps_preconfiguradas.

---
+++ streetlifting/apps/entrenamiento/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EntrenamientoConfig(AppConfig):
+    name = 'apps.entrenamiento'
---
+++ streetlifting/streetlifting/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for streetlifting project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('entrenamiento/', include('apps.entrenamiento.urls')),
+    #path('perfil/', include('apps.perfil.urls')),
+    #path('estadistica/', include('apps.estadistica.urls')),
+]
