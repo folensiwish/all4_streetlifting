883a742d9d1880dbb8db97260e693d8b41adef77
folensiwish <fer.albertoandres@gmail.com>
2026-03-25 21:15:22 -0300

    Creacion de los modelos y creacion del template home para mostrar un landing page

--- streetlifting/apps/entrenamiento/models.py
+++ streetlifting/apps/entrenamiento/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.utils import timezone # Importante para manejar zonas horarias
 # Create your models here.
+
+class Ejercicio(models.Model):
+    nombre_ejercicio = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.nombre_ejercicio
+
+class Sesion(models.Model):
+    fecha_sesion = models.DateField(default=timezone.now)
+    comentario = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'Sesion - {self.fecha_sesion}'
+
+class Serie(models.Model):
+    sesion = models.ForeignKey(Sesion, on_delete=models.CASCADE)
+    ejercicio = models.ForeignKey(Ejercicio, on_delete=models.CASCADE)
+
+    repeticion = models.PositiveSmallIntegerField()
+    peso_lastre = models.DecimalField(max_digits=5, decimal_places=2)
+    rpe = models.PositiveSmallIntegerField() #rpe es la escala de esfuerzo de la serie 
+    rir = models.PositiveSmallIntegerField() #rir son las repeticiones en reserva estimadas antes de llegar al fallo
+
+    def __str__(self):
+        return f'{self.ejercicio.nombre_ejercicio} - {self.peso_lastre}kg'
--- streetlifting/apps/entrenamiento/views.py
+++ streetlifting/apps/entrenamiento/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hola soy albertoooo')
+def crear_entrenamiento(request):
+    return render(request, 'entrenamiento/crear_entrenamiento.html', context={})
